89b32ae113c0a7239441334facdcccbdf56dfab9
Renzo <renzojacobtamayo@gmail.com>
2026-09-12 19:29:23 +0800

    Initial Commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar> <ion-title> My Photo Gallery </ion-title> </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <camera_component @photo-captured="addPhoto" />
+      <photo_gallery_component :photos="photos" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { 
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent
+} from "@ionic/vue";
+import { ref } from "vue";
+import camera_component from "@/components/CameraComponents.vue";
+import photo_gallery_component from "@/components/PhotoGallery.vue";
+const photos = ref<string[]>([]);
+const addPhoto = (photo: string) => {
+  photos.value.unshift(photo);
+};
+</script>
